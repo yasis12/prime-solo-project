7b5a0fe40278721877d029f610619ba021d8b7ef
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-13 14:31:17 -0500

    trying to get additional form fields to add on button click. Not currently working

--- src/components/IncomePage/IncomePage.jsx
+++ src/components/IncomePage/IncomePage.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './IncomePage.css'
 import { useHistory } from 'react-router-dom';
 
 function IncomePage() {
     const history = useHistory();
 
+    const [otherIncomeForm, setOtherIncomeForm] = useState([]);
+
     const handleSubmit = () => {
-        history.push('/needs')
+        history.push('/needs');
+    }
+
+    const addInputField = () => {
+        setOtherIncomeForm([...otherIncomeForm, { price: '', description: '' }]);
+    }
+
+    const handleOtherIncomeChange = (index, event) => {
+        const { name, value } = event.target;
+        const updatedFields = [...otherIncomeForm];
+        updatedFields[index][name] = value;
+        setOtherIncomeForm(updatedFields);
     }
     
     return (
@@ -18,9 +32,17 @@ function IncomePage() {
                 <input type="text" placeholder='Description' />
             </div>
             <div className='otherIncome form-field'>
-                <label>Other Income: </label>
-                <input type="number" />
-                <input type="text" placeholder='Description' />
+            <label>Other Income: </label>
+                {otherIncomeForm.map((field, index) => (
+                    <div key={index}>
+                        <input type="number" name='price' value={field.price} onChange={(event) => handleOtherIncomeChange(index, event)}/>
+                        <input type="text" placeholder='Description' name="description" value={field.description} onChange={(event) => handleOtherIncomeChange(index, event)}/>
+                        <button onClick={addInputField}> + </button>
+                    </div>
+    
+                ))}
+                
+            
             </div>
             <button type='submit'>NEXT PAGE</button>
         </form>
--- src/components/NeedsPage/NeedsPage.jsx
+++ src/components/NeedsPage/NeedsPage.jsx
@@ -1,9 +1,32 @@
 import './NeedsPage.css'
+import { useHistory } from 'react-router-dom';
 
 function NeedsPage() {
-    
+    const history = useHistory();
+
+    const handleSubmit = () => {
+        history.push('/wants')
+    }
     return (
-        <h1>Needs Page</h1>
+        <>
+         <h1>Needs Page</h1>
+         <form onSubmit={handleSubmit}>
+            <div className='rentMortgage form-field'>
+                <label>Rent | Mortgage: </label>
+                <input type="number" />
+                <input type="text" placeholder='Description' />
+            </div>
+            <div className='rent/homeInsurance form-field'>
+                <label>Rental Home Insurance: </label>
+                <input type="number" />
+                <input type="text" placeholder='Description' />
+            </div>
+            <button type='submit'>NEXT PAGE</button>
+        </form>
+
+        </>
+       
+
     )
 }
 
