6a186584f1861bddd0d526595f10fcd1e108a8b0
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-13 13:33:29 -0500

    Income page form created

--- src/components/IncomePage/IncomePage.jsx
+++ src/components/IncomePage/IncomePage.jsx
@@ -1,9 +1,31 @@
 import './IncomePage.css'
+import { useHistory } from 'react-router-dom';
 
 function IncomePage() {
+    const history = useHistory();
+
+    const handleSubmit = () => {
+        history.push('/needs')
+    }
     
     return (
-        <h1>Income Page</h1>
+        <>
+     <h1>Income Page</h1>
+        <form onSubmit={handleSubmit}>
+            <div className='wagesAfterTax form-field'>
+                <label>Wages After Tax: </label>
+                <input type="number" />
+                <input type="text" placeholder='Description' />
+            </div>
+            <div className='otherIncome form-field'>
+                <label>Other Income: </label>
+                <input type="number" />
+                <input type="text" placeholder='Description' />
+            </div>
+            <button type='submit'>NEXT PAGE</button>
+        </form>
+
+        </>
     )
 }
 
