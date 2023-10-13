d0a8bb6c1a0cea5a8d088a40610598801fcd1841
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-12 19:41:00 -0500

    Updated database with tables needed for application functionality

--- database.sql
+++ database.sql
@@ -8,3 +8,44 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "Income" (
+  "price" int NOT NULL,
+  "description" varchar,
+  "category" int NOT NULL,
+  "user_id" int NOT NULL,
+  "budget_id" int NOT NULL
+);
+
+CREATE TABLE "Needs" (
+  "price" int NOT NULL,
+  "description" varchar,
+  "category" int NOT NULL,
+  "user_id" int NOT NULL,
+  "budget_id" int NOT NULL
+);
+
+CREATE TABLE "Wants" (
+  "price" int NOT NULL,
+  "description" varchar,
+  "category" int NOT NULL,
+  "user_id" int NOT NULL,
+  "budget_id" int NOT NULL
+);
+
+CREATE TABLE "SavingsDebts" (
+  "price" int NOT NULL,
+  "description" varchar,
+  "category" int NOT NULL,
+  "user_id" int NOT NULL,
+  "budget_id" int NOT NULL
+);
+
+ALTER TABLE "Income" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+ALTER TABLE "Needs" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+ALTER TABLE "Wants" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+ALTER TABLE "SavingsDebts" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
