8dbe9a76d82e3655450815553b075946ed1a4ce4
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-14 14:04:54 -0500

    Adding additional fields now works as intended

--- src/components/IncomePage/IncomePage.jsx
+++ src/components/IncomePage/IncomePage.jsx
@@ -5,27 +5,29 @@ import { useHistory } from 'react-router-dom';
 function IncomePage() {
     const history = useHistory();
 
-    const [otherIncomeForm, setOtherIncomeForm] = useState([]);
+    const [otherIncomeForm, setOtherIncomeForm] = useState([{price:'', description: ''}]);
 
     const handleSubmit = () => {
         history.push('/needs');
     }
 
     const addInputField = () => {
+        event.preventDefault();
         setOtherIncomeForm([...otherIncomeForm, { price: '', description: '' }]);
+        console.log('Input Field being added');
     }
 
-    const handleOtherIncomeChange = (index, event) => {
-        const { name, value } = event.target;
+    const handleOtherIncomeChange = (event, i) => {
+        const { name, value } = event.target
         const updatedFields = [...otherIncomeForm];
-        updatedFields[index][name] = value;
+        updatedFields[i][name] = value;
         setOtherIncomeForm(updatedFields);
     }
     
     return (
         <>
      <h1>Income Page</h1>
-        <form onSubmit={handleSubmit}>
+         <form onSubmit={handleSubmit}>
             <div className='wagesAfterTax form-field'>
                 <label>Wages After Tax: </label>
                 <input type="number" />
@@ -33,20 +35,17 @@ function IncomePage() {
             </div>
             <div className='otherIncome form-field'>
             <label>Other Income: </label>
-                {otherIncomeForm.map((field, index) => (
-                    <div key={index}>
-                        <input type="number" name='price' value={field.price} onChange={(event) => handleOtherIncomeChange(index, event)}/>
-                        <input type="text" placeholder='Description' name="description" value={field.description} onChange={(event) => handleOtherIncomeChange(index, event)}/>
+                {otherIncomeForm.map((value, i) => (
+                    <div key={i}>
+                        <input type="number" name='price' value={value.price} onChange={(event) => handleOtherIncomeChange(event, i)}/>
+                        <input type="text" placeholder='Description' name="description" value={value.description} onChange={(event) => handleOtherIncomeChange(event, i)}/>
                         <button onClick={addInputField}> + </button>
                     </div>
     
                 ))}
-                
-            
             </div>
             <button type='submit'>NEXT PAGE</button>
         </form>
-
         </>
     )
 }
