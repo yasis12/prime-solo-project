075733cb9ff9d7ee59604c842be634fc9a103a42
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-14 16:20:39 -0500

    Completed the wants form

--- src/components/WantsPage/WantsPage.jsx
+++ src/components/WantsPage/WantsPage.jsx
@@ -1,19 +1,41 @@
 import './WantsPage.css'
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 function WantsPage() {
     const history = useHistory();
 
     //USE STATE
+    const [forms, setForms] = useState({
+        diningOut: [{ price: '', description: '' }],
+        clothingJewlery: [{ price: '', description: '' }],
+        drugs: [{ price: '', description: '' }],
+        moviesEvents: [{ price: '', description: '' }],
+        memberships: [{ price: '', description: '' }],
+        travel: [{ price: '', description: '' }],
+        subscriptions: [{ price: '', description: '' }],
+        homeDecor: [{ price: '', description: '' }],
+        other: [{ price: '', description: '' }]
+    })
 
+    // HANDLE + BUTTON CLICK
+    const addInputField = (category) => {
+        event.preventDefault();
+        setForms({
+            ...forms,
+            [category]: [...forms[category], { price: '', description: '' }],
+        });
+    };
 
-     // HANDLE + BUTTON CLICK
+    //HANDLE INPUT FIELD CHANGE
+    const handleInputChange = (event, category, index) => {
+        const {name, value} = event.target;
+        const updatedForms = { ...forms };
+        updatedForms[category][index][name] = value;
+        setForms(updatedForms);
+    }
 
-
-     //HANDLE INPUT FIELD CHANGE
-
-
-     //HANDLE FORM SUBMIT
+    //HANDLE FORM SUBMIT
     const handleSubmit = () => {
         history.push('/savingsdebts')
     }
@@ -24,26 +46,36 @@ function WantsPage() {
         <form onSubmit={handleSubmit}>
         {/* START FORM */}
             {/* Dining Out */}
-
-            {/* Clotihg & Jewlery */}
-
-            {/* Drugs (alcohol, THC) */}
-
-            {/* Movie / Events */}
-
-            {/* Memberships */}
-
-            {/* Travel */}
-
-            {/* Subscriptions */}
-
-            {/* Home decor Items */}
-
-            {/* Other */}
-        
-
+        {['diningOut','clothingJewlery','drugs','moviesEvents','memberships','travel','subscriptions','homeDecor','other'].map((category) => (
+            <div className={category} key={category}>
+                <label>
+                    {category === 'diningOut' ? 'Dining Out' : 
+                        category === 'clothingJewlery' ? 'Clothing & Jewlery' : 
+                        category === 'drugs' ? 'Drugs (Alcohol, THC)' :
+                        category === 'moviesEvents' ? 'Movies & Events' :
+                        category === 'memberships' ? 'Memberships' :
+                        category === 'travel' ? 'Travel' :
+                        category === 'subscriptions' ? 'Subscriptions' :
+                        category === 'homeDecor' ? 'Home Decor Items' :
+                        'Other Wants'}:
+                </label>
+                {forms[category].map((value, i) => (
+                    <div key={i}>
+                        <input 
+                        type = "number"
+                        name = "price"
+                        value ={value.price}
+                        onChange={(event) => handleInputChange(event, category, i)}
+                        />
+                        <button onClick={() => addInputField(category)}>+</button>
+                    </div>
+                ))}
+            </div>
+        ))}
+        <button type='submit'>NEXT PAGE</button>
         {/* END FORM */}
         </form>
+        
         </>
        
     )
