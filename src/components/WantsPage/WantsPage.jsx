f496144d97bbaecd69df381796944e4b485cc567
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-14 14:59:14 -0500

    Set up sections for Wants Page

--- src/components/WantsPage/WantsPage.jsx
+++ src/components/WantsPage/WantsPage.jsx
@@ -1,9 +1,51 @@
 import './WantsPage.css'
+import { useHistory } from 'react-router-dom';
 
 function WantsPage() {
-    
+    const history = useHistory();
+
+    //USE STATE
+
+
+     // HANDLE + BUTTON CLICK
+
+
+     //HANDLE INPUT FIELD CHANGE
+
+
+     //HANDLE FORM SUBMIT
+    const handleSubmit = () => {
+        history.push('/savingsdebts')
+    }
+
     return (
+        <>
         <h1>Wants Page</h1>
+        <form onSubmit={handleSubmit}>
+        {/* START FORM */}
+            {/* Dining Out */}
+
+            {/* Clotihg & Jewlery */}
+
+            {/* Drugs (alcohol, THC) */}
+
+            {/* Movie / Events */}
+
+            {/* Memberships */}
+
+            {/* Travel */}
+
+            {/* Subscriptions */}
+
+            {/* Home decor Items */}
+
+            {/* Other */}
+        
+
+        {/* END FORM */}
+        </form>
+        </>
+       
     )
 }
 
