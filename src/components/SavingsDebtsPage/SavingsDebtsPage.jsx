ad8a4704af2bb3db7a3d40ac94e5e76c5a994732
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-14 16:31:30 -0500

    Completed the Savings & Debts Form

--- src/components/SavingsDebtsPage/SavingsDebtsPage.jsx
+++ src/components/SavingsDebtsPage/SavingsDebtsPage.jsx
@@ -1,9 +1,83 @@
 import './SavingsDebtsPage.css'
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 function SavingsDebtsPage() {
+    const history = useHistory();
+
+    //USE STATE
+    const [forms, setForms] = useState({
+        emergancyFundContributions: [{ price: '', description: '' }],
+        savingsAccountsContributions: [{ price: '', description: '' }],
+        workRetirementContributions: [{ price: '', description: '' }],
+        individualRetirement: [{ price: '', description: '' }],
+        excessLoanPayments: [{ price: '', description: '' }],
+        creditCardPayment: [{ price: '', description: '' }],
+        studentLoan: [{ price: '', description: '' }],
+        other: [{ price: '', description: '' }],
+        debts: [{ price: '', description: '' }]
+    })
+
+    // HANDLE + BUTTON CLICK
+    const addInputField = (category) => {
+        event.preventDefault();
+        setForms({
+            ...forms,
+            [category]: [...forms[category], { price: '', description: '' }],
+        });
+    };
+
+    //HANDLE INPUT FIELD CHANGE
+    const handleInputChange = (event, category, index) => {
+        const {name, value} = event.target;
+        const updatedForms = { ...forms };
+        updatedForms[category][index][name] = value;
+        setForms(updatedForms);
+    }
+
+    //HANDLE FORM SUBMIT
+    const handleSubmit = () => {
+        history.push('/audit')
+    }
+
     
     return (
-        <h1>Savings & Debts Page</h1>
+        <>
+        <h1>Savings & Debts</h1>
+        <form onSubmit={handleSubmit}>
+        {/* START FORM */}
+            {/* Dining Out */}
+        {['emergancyFundContributions','savingsAccountsContributions','workRetirementContributions','individualRetirement','excessLoanPayments','creditCardPayment','studentLoan','other','debts'].map((category) => (
+            <div className={category} key={category}>
+                <label>
+                    {category === 'emergancyFundContributions' ? 'Emergancy Fund Contributions' : 
+                        category === 'savingsAccountsContributions' ? 'Savings Account Contributions' : 
+                        category === 'workRetirementContributions' ? '401k Contributions' :
+                        category === 'individualRetirement' ? 'Individual Retirement Account Contributions' :
+                        category === 'excessLoanPayments' ? 'Excess Loan Payments' :
+                        category === 'creditCardPayment' ? 'Credit Card Minimum Monthly Payments' :
+                        category === 'studentLoan' ? 'Student Loan Minimum Monthly Payments' :
+                        category === 'other' ? 'Other Savings' :
+                        'Debts'}:
+                </label>
+                {forms[category].map((value, i) => (
+                    <div key={i}>
+                        <input 
+                        type = "number"
+                        name = "price"
+                        value ={value.price}
+                        onChange={(event) => handleInputChange(event, category, i)}
+                        />
+                        <button onClick={() => addInputField(category)}>+</button>
+                    </div>
+                ))}
+            </div>
+        ))}
+        <button type='submit'>NEXT PAGE</button>
+        {/* END FORM */}
+        </form>
+        
+        </>
     )
 }
 
