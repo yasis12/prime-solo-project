3c162b2aa85b833902e2665a44c00b65c2f340d5
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-18 20:37:14 -0500

    added data to the review page

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,9 +1,16 @@
 import './ReviewPage.css'
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function ReviewPage() {
+    // STORE
+    const incomeArray = useSelector(store => store.income);
+    const needsArray = useSelector(store => store.needs);
+    const wantsArray = useSelector(store => store.wants);
+    const savingsDebtsArray = useSelector(store => store.savingsDebts);
+
     const history = useHistory();
-    // JSON.stringify()
+ 
     const handleSubmit =() => {
         history.push('/audit')
     }
@@ -11,6 +18,20 @@ function ReviewPage() {
     return (
         <>
         <h1>Review Page</h1>
+        <h3>Income</h3>
+        <h5>{JSON.stringify(incomeArray)}</h5>
+        <br />
+        <h3>Needs</h3>
+        <h5>{JSON.stringify(needsArray)}</h5>
+        <br />
+        <h3>Wants</h3>
+        <h5>{JSON.stringify(wantsArray)}</h5>
+        <br />
+        <h3>Savings & Debts</h3>
+        <h5>{JSON.stringify(savingsDebtsArray)}</h5>
+        <br />
+
+
 
         <button onClick={handleSubmit}>SUBMIT DATA / NEXT PAGE</button>
         </>
