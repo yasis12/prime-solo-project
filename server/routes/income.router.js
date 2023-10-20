32a189e8ca1c88a1c8193f4791c609f5d46cd9d1
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-19 19:17:00 -0500

    Created the income Router

---
+++ server/routes/income.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// income POST ROUTER
+router.post('/income', (req, res) => {
+    // POST route code here
+  });
+
+// Income GET route
+router.get('/income', (req, res) => {
+    // GET route code here
+  });
+
+  module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const incomeRouter = require('./routes/income.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/income', incomeRouter);
 
 // Serve static files
 app.use(express.static('build'));
