202e487962005aa87eafe30486924602dc0423f5
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-19 20:06:20 -0500

    Income post route works properly

--- server/routes/income.router.js
+++ server/routes/income.router.js
@@ -4,14 +4,63 @@ const router = express.Router();
 
 // income POST ROUTER
 router.post('/', (req, res) => {
-    console.log('Income POST route reached');
     // POST route code here
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('User', req.user.id);
     const { incomeData, budgetID } = req.body;
     console.log('Income Data', incomeData);
     console.log('Budget ID', budgetID);
 
+    const userID = req.user.id;
 
+    // Database insertion logic
+    const insertPromises = [];
+        // Insert wages after tax
+        insertPromises.push(
+            pool.query(
+                'INSERT INTO "Income" ("price", "description", "category", "user_id", "budget_id") VALUES($1, $2, $3, $4, $5)',
+                [
+                    parseInt(incomeData.wagesAfterTax.price),
+                    incomeData.wagesAfterTax.description,
+                    'wagesAfterTax', // Replace with appropriate category value
+                    userID,
+                    budgetID,
+                ]
+            )
+        );
+
+    // Insert otherIncomeForm
+    if (incomeData.otherIncomeForm.length > 0) {
+        incomeData.otherIncomeForm.forEach((item) => {
+            if (item.price) {
+                insertPromises.push(
+                    pool.query(
+                        'INSERT INTO "Income" ("price", "description", "category", "user_id", "budget_id") VALUES($1, $2, $3, $4, $5)',
+                        [
+                            parseInt(item.price),
+                            item.description,
+                            'otherIncome', // Replace with appropriate category value
+                            userID,
+                            budgetID,
+                        ]
+                    )
+                );
+            }
+        });
+    }
+
+    Promise.all(insertPromises)
+        .then(() => {
+            console.log('Data inserted successfully');
+            res.sendStatus(201); // Created
+        })
+        .catch((error) => {
+            console.error('Error inserting data:', error);
+            res.status(500).json({ error: 'An error occurred' });
+        });
   });
+
+
 
 // Income GET route
 router.get('/income', (req, res) => {
