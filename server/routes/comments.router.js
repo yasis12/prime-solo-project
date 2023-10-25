d5344004bf05e43645109359cdb6e7a64655ddb0
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-24 20:41:20 -0500

    Comments is working kinda but not as inteneded. Multiple budgets is hard to deal with

--- server/routes/comments.router.js
+++ server/routes/comments.router.js
@@ -9,7 +9,24 @@ router.get('/', (req, res) => {
 
 
 router.post('/', (req, res) => {
-  // POST route code here
+    const { comment, budgetID } = req.body;
+    const userID = req.user.id;
+
+    console.log('comment', comment);
+    console.log('budget ID', budgetID);
+    console.log('userID', userID);
+    
+    pool.query(
+        `INSERT INTO "BudgetComments" ("comments", "user_id", "budget_id") VALUES ($1, $2, $3)`,
+        [comment, userID, budgetID]
+    )
+    .then(result => {
+        res.status(201).json({ message: 'Comment saved successfully', budgetID, comment });
+    })
+    .catch(error => {
+        console.error('Error saving Comment', error);
+        res.status(500)
+    });
 });
 
 router.delete('/:id', (req,res) => {
--- src/components/BudgetComments/BudgetComments.jsx
+++ src/components/BudgetComments/BudgetComments.jsx
@@ -1,15 +1,29 @@
 import './BudgetComments.css'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { useSelector} from 'react-redux';
 
 function BudgetComments() {
 
+    const budgetID = useSelector(store => store.budgetID)
     const [budgets, setBudgets] = useState([]);
+    const [commentToPost, setCommentToPost] = useState('');
+    const [comments, setComments] = useState('');
 
     const fetchBudgets = () => {
         axios.get('/api/budgetID/all').then((response) => {
           console.log('Fetched Budgets', response.data);
             setBudgets(response.data);
+        }).catch((error) => {
+          console.log('error fetching budgets', error);
+          alert('Something went wrong.');
+        });
+      }
+
+      const fetchComments = () => {
+        axios.get('/api/comments').then((response) => {
+          console.log('Fetched comments', response.data);
+          setComment(response.data);
         }).catch((error) => {
           console.log('error fetching budgets', error);
           alert('Something went wrong.');
@@ -22,24 +36,43 @@ function BudgetComments() {
     
     const handleSubmit = () => {
 
+      const requestData = {
+        comment,
+        budgetID: parseInt(budgetID)
+      }
+      console.log('Post Request Data', requestData);
+      axios.post('/api/comments', requestData)
+        .then((response) => {
+            console.log(`Comment data submitted successfully`);
+        }).catch(error => {
+            console.log('Error submitting Comment data', error);
+        });
     };
 
     return (
-        <>
-        <h1>Comment any budget insights you have here!</h1>
-        <ul>
-            {budgets.map((budget) => (
-               <li key={budget.id} value={budget.id}>
-               {budget.budgetTitle}
-               <br />
-               <form onSubmit={handleSubmit}>
-                <input type="text" />
-               </form>
-           </li> 
-            ))}
-            
-        </ul>
-        </>
+      <>
+      <h1>Comment any budget insights you have here!</h1>
+      {budgets.map((budget) => (
+        <div key={budget.id}>
+          <h2>{budget.budgetTitle}</h2>
+          <ul>
+            {/* {comments
+              .filter((comment) => comment.budgetID === budget.id)
+              .map((comment) => (
+                <li key={comment.id}>
+                  {comment.comment}
+                  <button onClick={() => handleEditComment(comment.id)}>Edit</button>
+                  <button onClick={() => handleDeleteComment(comment.id)}>Delete</button>
+                </li>
+              ))} */}
+          </ul>
+          <form onSubmit={() => handleSubmit(budget.id)}>
+            <input type="text" value={commentToPost} onChange={(event) => setCommentToPost(event.target.value)} />
+            <button type="submit">Add Comment</button>
+          </form>
+        </div>
+      ))}
+    </>
     )
 }
 
