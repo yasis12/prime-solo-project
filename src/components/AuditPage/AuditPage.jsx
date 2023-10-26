0b9d1b4fc1262ea52893222f44173815d72b881c
Rafael Yasis <rafaelyasis@gmail.com>
2023-10-25 20:55:21 -0500

    Moved portion of aduit page to its own component

--- src/components/AuditPage/AuditPage.jsx
+++ src/components/AuditPage/AuditPage.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import './AuditPage.css'
 import axios from 'axios';
 import { number } from 'prop-types';
+import CategorizedSpending from '../CategorizedSpending/CategorizedSpending';
 
 function AuditPage() {
 
@@ -163,25 +164,8 @@ function AuditPage() {
             </div>
         </div>
 
-        {/* 
-        This section is inteneded to help the user see what category they are spending the most money on and how it compares to their othe expenses
-        */}
-        <div className='categorizedSpending'>
-            <div id='categorySpendingTitle'>
-                <h3>What category are you over spending in?</h3>
-            </div>
-            <div>
-                {/* I will create a new component that has the functionality that I desire here but for now I will add the below code as a place holder so I can see what it will look like*/}
-                <h5>rent utilities: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-                <h5>gas: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-                <h5>Groceries: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-                <h5>shopping: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-                <h5>dining out: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-                <h5>fun: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-                <h5>Household Misc: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
-            </div>
-        </div>
-
+        {/* <CategorizedSpending />  This is a stretch goal now*/}
+        
         {/* This section will demonstrate (with out interest) how long it will take to pay off current debts. If there are no current debts it well say congragts */}
         <div className='debtCalculator'>
             <div id='moneyLeftEOM'>
---
+++ src/components/CategorizedSpending/CategorizedSpending.jsx
@@ -0,0 +1,30 @@
+
+function CategorizedSpending() {
+  
+    return (
+        <>
+        {/* 
+        This section is inteneded to help the user see what category they are spending the most money on and how it compares to their othe expenses
+        */}
+        <div className='categorizedSpending'>
+            <div id='categorySpendingTitle'>
+                <h3>What category are you over spending in?</h3>
+            </div>
+            <div>
+                {/* I will create a new component that has the functionality that I desire here but for now I will add the below code as a place holder so I can see what it will look like*/}
+                <h5>rent utilities: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+                <h5>gas: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+                <h5>Groceries: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+                <h5>shopping: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+                <h5>dining out: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+                <h5>fun: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+                <h5>Household Misc: &nbsp;&nbsp;&nbsp;&nbsp;  $500  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  %20 </h5>
+            </div>
+        </div>
+
+        
+        </>
+    )
+}
+
+export default CategorizedSpending;
